3b6752985c3cf50312cf440ada24fd63519dfc08
Nitish Kumar Sharma <nitish.sharma@thoughtworks.com>
2020-02-11 16:56:09 +0530

    TicTacToe can now declare Player B as winner

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -22,4 +22,19 @@ class TicTacToeTest {
 
         Assertions.assertEquals(GameStatus.PLAYER_X_WIN, ticTacToe.getGameStatus());
     }
+
+    @Test
+    void shouldShowPlayerBAsWinner() throws InvalidMoveExcception {
+        Grid grid = mock(Grid.class);
+        TicTacToe ticTacToe = new TicTacToe(grid);
+        Player playerO = mock(Player.class);
+        Player playerX = mock(Player.class);
+        when(grid.checkStatus()).thenReturn(GameStatus.PLAYER_O_WIN);
+        when(playerX.toMove()).thenReturn(new Position(0, 0, Move.X), new Position(0, 1, Move.X), new Position(2, 2, Move.X));
+        when(playerO.toMove()).thenReturn(new Position(1, 0, Move.O), new Position(1, 1, Move.O), new Position(1, 2, Move.O));
+
+        ticTacToe.play(playerX, playerO);
+
+        Assertions.assertEquals(GameStatus.PLAYER_O_WIN, ticTacToe.getGameStatus());
+    }
 }
